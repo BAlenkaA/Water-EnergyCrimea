c88a38aecc90298cbdb034780832a497d6b3bd97
alena <alena@belintsev.ru>
2024-11-11 16:55:52 +0300

    Изменен парсинг электричества, данные добавляются в бд. Добавлен планировщик запуска задач

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import datetime
 import logging
+import time
 
 import requests_cache
+import schedule
 
 from configs import configure_logging
 from db import create_database
-from energy import get_url_energy, send_energy_message
+from energy import check_energy_repair_work
 from exceptions import ParserFindTagException
 from utils import send_telegram_message
-from water import check_water_maintenance
+from water import check_water_repair_work
 
 
 def main():
@@ -18,17 +20,19 @@ def main():
     create_database()
     session = requests_cache.CachedSession()
     session.cache.clear()
-
+    today = datetime.date.today()
     try:
         for day in [
-            datetime.date.today().strftime('%d'),
-            (datetime.datetime.today() + datetime.timedelta(days=1)).strftime('%d')
+            today.strftime('%d'),
+            (today + datetime.timedelta(days=1)).strftime('%d')
         ]:
-            water_message = check_water_maintenance(day, session)
+            water_message = check_water_repair_work(day, session)
             if water_message:
                 send_telegram_message(water_message, session)
-        # link = get_url_energy(session)
-        # send_energy_message(link, session)
+        energy_message = check_energy_repair_work(today, session)
+        if energy_message:
+            send_telegram_message(energy_message, session)
+
         logging.info('Парсер ждет перезапуска!')
     except ParserFindTagException as e:
         logging.error(f'Ошибка при выполнении парсинга: {e}')
@@ -36,5 +40,10 @@ def main():
         logging.exception(f'Произошла ошибка: {e}', stack_info=True)
 
 
+for hour in range(9, 20):
+    schedule.every().day.at(f"{hour:02}:00").do(main)
+
 if __name__ == "__main__":
-    main()
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
